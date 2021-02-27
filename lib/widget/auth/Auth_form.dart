d7d9b454b4ff52d33fa059bd5469d27e8413f246
Mohamed Rashad <mohamedrashad9797@gmail.com>
2021-02-27 02:32:53 +0200

    all project

---
+++ lib/widget/auth/Auth_form.dart
@@ -0,0 +1,147 @@
+import 'dart:io';
+
+import '../pickers/user_image_picker.dart';
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  final void Function(String email, String password, String username,
+      File image, bool isLogin, BuildContext ctx) submitFn;
+  final bool isLoading;
+
+  AuthForm(this.submitFn, this.isLoading);
+
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm>
+    with SingleTickerProviderStateMixin {
+  final _formKey = GlobalKey<FormState>();
+  bool _isLogin = true;
+  String _email = "";
+  String _password = "";
+  String _username = "";
+  File _userImageFile;
+
+  void _pickedImage(File pickedImage) {
+    _userImageFile = pickedImage;
+  }
+
+  void _submit() {
+    final isValid = _formKey.currentState.validate();
+    FocusScope.of(context).unfocus();
+
+    if (!_isLogin && _userImageFile == null) {
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text("Please pick an image."),
+        backgroundColor: Theme.of(context).errorColor,
+      ));
+      return;
+    }
+
+    if (isValid) {
+      _formKey.currentState.save();
+      widget.submitFn(_email.trim(), _password.trim(), _username.trim(),
+          _userImageFile, _isLogin, context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        margin: EdgeInsets.all(20),
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 300),
+          height: _isLogin ? 300 : 500,
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  if (!_isLogin) UserImagePicker(_pickedImage),
+                  TextFormField(
+                    autocorrect: false,
+                    enableSuggestions: false,
+                    textCapitalization: TextCapitalization.none,
+                    key: ValueKey('email'),
+                    validator: (val) {
+                      if (val.isEmpty || !val.contains('@')) {
+                        return "Please enter a valid email address";
+                      }
+                      return null;
+                    },
+                    onSaved: (val) => _email = val,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.person),
+                        hintText: 'Enter your email',
+                        labelText: 'Email'),
+                  ),
+                  if (!_isLogin)
+                    TextFormField(
+                      autocorrect: true,
+                      enableSuggestions: false,
+                      textCapitalization: TextCapitalization.words,
+                      key: ValueKey('username'),
+                      validator: (val) {
+                        if (val.isEmpty || val.length < 4) {
+                          return "Please enter at least 4 characters";
+                        }
+                        return null;
+                      },
+                      onSaved: (val) => _username = val,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.person),
+                          hintText: 'Enter username',
+                          labelText: 'Username'),
+                    ),
+                  TextFormField(
+                    key: ValueKey('password'),
+                    validator: (val) {
+                      if (val.isEmpty || val.length < 7) {
+                        return "Password must be at least 7 characters";
+                      }
+                      return null;
+                    },
+                    onSaved: (val) => _password = val,
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.visibility_off),
+                        suffixIcon: Icon(Icons.visibility),
+                        hintText: 'Enter your password',
+                        labelText: 'Password'),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 12),
+                  if (widget.isLoading) CircularProgressIndicator(),
+                  if (!widget.isLoading)
+                    RaisedButton(
+                      child: Text(_isLogin ? 'Login' : 'Sign Up'),
+                      onPressed: _submit,
+                    ),
+                  if (!widget.isLoading)
+                    FlatButton(
+                      textColor: Theme.of(context).primaryColor,
+                      child: Text(_isLogin
+                          ? 'Create new account'
+                          : 'I already have an account'),
+                      onPressed: () {
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      },
+                    ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
